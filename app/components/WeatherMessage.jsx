48e335705d5198d0a3372c04bed2fe185b86b51a
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-07-19 17:58:02 -0500

    Fake api call - Pass data through props and set state (data flow)

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
   render: function() {
+    var {location, temp} = this.props;
+
     return (
-      <h3>WeatherMessage Component</h3>
+      <h3>It is {temp} in {location}</h3>
     );
   }
 });
