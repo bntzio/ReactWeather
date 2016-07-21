12236242aece4508dca54242700f25ac71d30513
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-07-20 22:56:57 -0500

    Style About component

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a simple weather web app created by <a href="https://bntz.io" target="_blank">bntz.io</a> using react.js</p>
+    </div>
   )
 };
 
