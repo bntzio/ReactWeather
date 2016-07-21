bea6f3e3a6e8c1581eb082ba1793db0a75b4200c
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-07-20 23:07:00 -0500

    Style Weather components

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h3>It is {temp} in {location}</h3>
+    <h3 className="text-center">It's {temp}º celsius in {location}.</h3>
   );
 };
 
